f4c5cf51ff87e171cfe2ad9aca5b2ccdd10c56a5
Banjer71 <davgio7@gmail.com>
2019-11-07 19:05:03 +0000

    Updates

---
+++ precache-manifest.fb81a8188c13214de60d089bfbb261e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cf059394095ae35b0b34336286920f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "fbf043891253fb78b383",
+    "url": "/my-app/static/css/main.0907a61d.chunk.css"
+  },
+  {
+    "revision": "869e0d66bb7a8aefd704",
+    "url": "/my-app/static/js/2.db769c3c.chunk.js"
+  },
+  {
+    "revision": "fbf043891253fb78b383",
+    "url": "/my-app/static/js/main.eb3cfda2.chunk.js"
+  },
+  {
+    "revision": "8d47c76e8f1fcd2f0a1b",
+    "url": "/my-app/static/js/runtime-main.c342a576.js"
+  }
+]);
