f81a15f6addda1db2aba00f468955b77fe804029
Banjer71 <davgio7@gmail.com>
2019-11-08 08:33:34 +0000

    Updates

---
+++ precache-manifest.0ac0cf92df7ab0cdcd85b568ed6f9c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436ce5b16c0950a163cb4f86adf55c2a",
+    "url": "/lyrics_bites/index.html"
+  },
+  {
+    "revision": "4f3492475bb673955f45",
+    "url": "/lyrics_bites/static/css/main.ddbe7bc9.chunk.css"
+  },
+  {
+    "revision": "869e0d66bb7a8aefd704",
+    "url": "/lyrics_bites/static/js/2.db769c3c.chunk.js"
+  },
+  {
+    "revision": "4f3492475bb673955f45",
+    "url": "/lyrics_bites/static/js/main.ef82621a.chunk.js"
+  },
+  {
+    "revision": "91029cf4daff29af7439",
+    "url": "/lyrics_bites/static/js/runtime-main.d4397d91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.6497dc94fb1f307354110dcf319f377d.js"
+  "/lyrics_bites/precache-manifest.0ac0cf92df7ab0cdcd85b568ed6f9c81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lyrics_bites/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
